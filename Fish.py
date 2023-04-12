4a117b5ce09a220a865f49b58ab51c35d852fe51
bedovlabs <bedo.virtual.labs@gmail.com>
2023-04-12 15:36:12 +0200

    user_input prediction fish

--- Fish.py
+++ Fish.py
@@ -10,13 +10,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import pickle
-
+import streamlit as st
 
 class fish_weight_prediction(object):
     # First, let's load the data
 
     def fish_weight_load_explore_split_data():
         df_fishw= pd.read_csv('data/fish.csv')
+       
         #df_RPP_Original=df_fishw.copy(deep=True)
         #df_fishw.drop('No',inplace=True,axis=1)
         df_fishw.columns = ['Species', 'Length1', 'Length2', 'Length3', 'Height', 'Width', 'Weight']
@@ -76,6 +77,20 @@ class fish_weight_prediction(object):
         pickle.dump(model_GBR, open(filename, 'wb'))
         return mse_GBR,rmse_GBR,r2_GBR
        
+    def user_input_feature_fish(Fish_co1,Fish_col2):
+        #['Species', 'Length1', 'Length2', 'Length3', 'Height', 'Width', 'Weight']
 
-   
+        Species = Fish_co1.selectbox('Species', options=[ 'Bream', 'Roach', 'Whitefish', 'Parkki', 'Perch', 'Pike' ,'Smelt'])
+        Length1 = Fish_co1.slider('Length1', 0, 1650, 1)
+        Length2 = Fish_co1.slider('Length2',5.0,65.0, 0.1)
+        Length3 = Fish_col2.slider('Length3', 5.0,65.0, 0.1)
+        Height=Fish_col2.slider('Height', 5.0,85.0, 0.1)
+        Width=Fish_col2.slider('Width',1.000, 25.000, step=0.001)
+        data = {'Species': Species,'Length1': Length1,
+            'Length2': Length2,'Length3': Length3,
+            'Height': Height, 'Width': Width}
+        features = pd.DataFrame(data, index=[0])
+        return features
+
+
 
