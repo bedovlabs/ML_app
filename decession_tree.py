9c0c02d9e293bfdb129f93c72d132254c969a854
bedovlabs <bedo.virtual.labs@gmail.com>
2023-04-13 11:43:58 +0200

    dog cat small

--- decession_tree.py
+++ decession_tree.py
@@ -292,7 +292,7 @@ class Supervised_decession_tree(object):
             cat_dog_classifier=image_dectree()
             x,y,df=cat_dog_classifier.load_data()
             datadir='media/animals/' 
-            for i in ['Cats','Dogs']:
+            for i in ['cats','dogs']:
                 path=os.path.join(datadir,i)
                 for img,j in zip(os.listdir(path),range(1,4)):
                   if i=='Cats':
